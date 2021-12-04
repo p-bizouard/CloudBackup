0f3f8c90b898aced15ebaf8d35d7c75a464d7106
Paul Bizouard <bizouap@gmail.com>
2021-12-04 20:34:47 +0100

    :children_crossing: Add ability to duplicate storages

--- src/Controller/Admin/StorageCrudController.php
+++ src/Controller/Admin/StorageCrudController.php
@@ -3,14 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Storage;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class StorageCrudController extends AbstractCrudController
 {
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Storage::class;
@@ -26,6 +35,18 @@ class StorageCrudController extends AbstractCrudController
         ;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        $copyEntity = Action::new('copyEntity', 'Dupliquer')
+            ->linkToCrudAction('copyEntity');
+
+        return $actions
+            ->add(Crud::PAGE_DETAIL, $copyEntity)
+            ->add(Crud::PAGE_EDIT, $copyEntity)
+            ->add(Crud::PAGE_INDEX, $copyEntity)
+        ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -38,11 +59,32 @@ class StorageCrudController extends AbstractCrudController
 
                 return $return;
             }),
+            AssociationField::new('backupConfigurations')->hideOnForm(),
             AssociationField::new('osProject'),
-            AssociationField::new('backupConfigurations'),
             TextField::new('osRegionName'),
-            TextField::new('resticPassword')->hideOnIndex(),
-            TextField::new('resticRepo'),
+            TextField::new('resticPassword')->hideOnIndex()->setRequired(true),
+            TextField::new('resticRepo')->setRequired(true),
         ];
+    }
+
+    public function copyEntity(AdminContext $context)
+    {
+        /** @var Storage */
+        $storage = $context->getEntity()->getInstance();
+        $newstorage = clone $storage;
+
+        $newstorage->setName(sprintf('Copie de %s', $newstorage->getName()));
+
+        $entityManager = $this->getDoctrine()->getManagerForClass(self::getEntityFqcn());
+        $entityManager->persist($newstorage);
+        $entityManager->flush();
+
+        $url = $this->adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Action::EDIT)
+            ->setEntityId($newstorage->getId())
+            ->generateUrl();
+
+        return new RedirectResponse($url);
     }
 }
