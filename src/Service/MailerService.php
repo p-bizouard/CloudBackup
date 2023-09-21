f537ad9c1200132521294fcb831aca56da68ef83
Paul Bizouard <bizouap@gmail.com>
2023-09-22 01:08:38 +0200

    🥅 (log) Refactor MailerService to handle email sending exceptions

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\Backup;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
@@ -12,27 +13,32 @@ class MailerService
     public function __construct(
         private string $from_address,
         private array $report_addresses,
-        private MailerInterface $mailer
+        private MailerInterface $mailer,
+        private LoggerInterface $logger,
     ) {
     }
 
     public function sendFailedBackupReport(Backup $backup): void
     {
-        $email = new Email();
-        $email
-            ->from(new Address($this->from_address))
-            ->returnPath($this->from_address)
-            ->subject(sprintf('[CloudBackup] %s failed', $backup->getName()))
-            ->html(sprintf("Backup %s failed.\nLast logs : %s",
-                $backup->getName(),
-                $backup->getLogsForReport()
-            ))
-        ;
+        try {
+            $email = new Email();
+            $email
+                ->from(new Address($this->from_address))
+                ->returnPath($this->from_address)
+                ->subject(sprintf('[CloudBackup] %s failed', $backup->getName()))
+                ->html(sprintf("Backup %s failed.\nLast logs : %s",
+                    $backup->getName(),
+                    $backup->getLogsForReport()
+                ))
+            ;
 
-        foreach ($this->report_addresses as $address) {
-            $email->addTo($address);
+            foreach ($this->report_addresses as $address) {
+                $email->addTo($address);
+            }
+
+            $this->mailer->send($email);
+        } catch (\Exception $e) {
+            $this->logger->error(sprintf('Cannot send email [%s]', $e->getMessage()));
         }
-
-        $this->mailer->send($email);
     }
 }
