296d69ecae0cff28a4fbe26b228c25ab82154ee9
Paul Bizouard <paul.bizouard@centralesupelec.fr>
2026-05-06 03:00:23 +0200

    migration: ✏️ fix typo

--- migrations/Version20260302091258.php
+++ migrations/Version20260302091258.php
@@ -20,12 +20,12 @@ final class Version20260302091258 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `host` ADD ssh_options TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE host ADD ssh_options TEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `host` DROP ssh_options');
+        $this->addSql('ALTER TABLE host DROP ssh_options');
     }
 }
