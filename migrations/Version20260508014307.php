8bfb8a92b2bc9fd99f215aad8389dfd304f8cfc3
Paul Bizouard <paul.bizouard@centralesupelec.fr>
2026-05-08 03:43:35 +0200

    feat(migration): add skip_rclone_check column to backup_configuration

---
+++ migrations/Version20260508014307.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260508014307 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE backup_configuration ADD skip_rclone_check BOOLEAN DEFAULT false NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE backup_configuration DROP skip_rclone_check');
+    }
+}
